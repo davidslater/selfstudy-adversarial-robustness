f7536f019a2f7d9e85065c9836bea16474616734
David Slater <david.slater@twosixlabs.com>
2021-05-24 10:39:26 -0700

    update

--- defense_temperature/attack_linf_torch.py
+++ defense_temperature/attack_linf_torch.py
@@ -16,12 +16,74 @@
 
 import common.framework
 
+import torch
+
+from defense_temperature.task_definition import LINF_THRESHOLD as EPS
+
 
 class LinfAttack(common.framework.Attack):
 
-    def attack(self, model, x, y):
+    def attack(self, model, x, y, eps=EPS, steps=10, restarts=5):
+        proxy = torch.nn.Sequential(*model.convnet.layers[:-1])
+        
+        x_orig = torch.tensor(x)
+        x = torch.tensor(x)
+        y = torch.LongTensor(y)
+        alpha = 0.000001
+        loss = torch.nn.CrossEntropyLoss()
+        eps_step = eps / 2
+       
+        # Test Benign
+        output = proxy(x)
+        failures = output.argmax(axis=1) == y
 
-        return x
+        x_full = x
+        y_full = y
+        for i in range(steps * (restarts + 1)):
+            print("Failures: ", failures.sum().numpy())
+            if not any(failures):
+                break
+            
+            x = x_full[failures]
+            y = y_full[failures]
+            if i % steps == 0 and i > 0:
+                # add random restart
+                print("Restart")
+                x = x_orig[failures] + 2*eps*(torch.rand(x.shape) - 0.5)
+
+            x.requires_grad = True
+            output = alpha * proxy(x)
+            loss(output, y).backward()
+            print(f"step {i}: output L1 = {output.abs().mean().detach().numpy()}, grad L1 = {x.grad.abs().mean().detach().numpy()}")
+            x = x.detach() + torch.sign(x.grad) * eps_step
+            x = torch.max(torch.min(x, x_orig[failures] + eps), x_orig[failures] - eps)
+            x_full[failures] = x
+
+            # check for failures
+            failures[failures.clone()] = proxy(x).argmax(axis=1) == y
+
+        x = x_full
+        # for i in range(steps)
+        #     x.requires_grad = True
+        #     output = alpha * proxy(x)
+        #     loss(output, y).backward()
+        #     print(f"step {i}: output L1 = {output.abs().mean().detach().numpy()}, grad L1 = {x.grad.abs().mean().detach().numpy()}")
+        #     x = x.detach() + torch.sign(x.grad) * eps_step
+        #     x = torch.max(torch.min(x, x_orig + eps), x_orig - eps)
+        
+        
+
+        #loss(alpha * output, y).backward()
+        #for i in range(len(x)):
+        #    output = model.convnet(x[i:i+1])
+        #    l = loss(model.convnet(x[i:i+1]), y[i:i+1])
+        #    l2 = loss2(model.convnet(x[i:i+1]), y[i:i+1])
+        #    print(i, l.detach(), l2.detach(), output.detach(), y[i])
+        #loss(model.convnet(x), y).backward()
+        
+        #print(x.grad.mean(), x.grad.std())
+
+        return x.numpy()
 
 # # If writing attack which operates on batch of examples is too complicated
 # # then remove LinfAttack and uncommend LinfAttackNonBatched from below:
